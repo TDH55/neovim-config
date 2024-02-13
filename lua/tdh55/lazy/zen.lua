4f0015648a6a26bb1b054267895da3cdaebe05ae
TDH55 <taylorhoward1@wustl.edu>
2024-02-12 18:08:16 -0600

    add zen and twiglight plugins

---
+++ lua/tdh55/lazy/zen.lua
@@ -0,0 +1,10 @@
+-- This plugin opens a floating window with a distraction-free buffer and removes all UI elements.
+return {
+  "folke/zen-mode.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  event = "VeryLazy"
+}
