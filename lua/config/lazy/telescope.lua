630419dcbb2ffddeb1bf0e782bc79a922a9a5336
tdh55 <thoward@luckyorange.com>
2024-02-26 10:41:55 -0600

    add neoscroll

--- lua/config/lazy/telescope.lua
+++ lua/config/lazy/telescope.lua
@@ -7,7 +7,8 @@ return {
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>pf', builtin.find_files, {})                         -- <leader>pf to search all files
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})                               -- <Ctrl-p> to search git files
+    vim.keymap.set('n', '<leader>ff', builtin.git_files, {})                         -- <leader>pf to search all files
+    -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})                               -- <Ctrl-p> to search git files
     vim.keymap.set('n', '<leader>fb', '<cmd>Telescope current_buffer_fuzzy_find<CR>') -- <leader>fb to search in current buffer (similar to <Cmd-f>)
     vim.keymap.set('n', '<leader>fa', '<cmd>Telescope live_grep<CR>')                 -- <leader>fa to search in all files (similar to <Cmd-Shift-f>)
     vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})                     -- <leader>fr When the cursor is on a symbol, search the references of the symbol
