a353f32503685a6523b9a94945390466a8d5b24f
TDH55 <taylorhoward1@wustl.edu>
2024-05-12 15:43:54 -0500

    add supermaven

--- lua/config/lazy/copilot.lua
+++ lua/config/lazy/copilot.lua
@@ -1,6 +1,7 @@
 return {
   -- Github Copilot, currently setup to use <Ctrl-TAB> to accept the suggestion (tab works if no lsp suggestions are presented)
   'github/copilot.vim',
+  enabled = false,
   config = function()
     vim.keymap.set('i', '<C-Tab>', 'copilot#Accept("\\<CR>")', {
       expr = true,
