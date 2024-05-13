55ca7c4806a7ea5537d3a17c820ecd1613477bdb
TDH55 <taylorhoward1@wustl.edu>
2024-05-12 20:08:40 -0500

    set supermaven accept suggestion keymap

--- lua/config/lazy/supermaven.lua
+++ lua/config/lazy/supermaven.lua
@@ -2,6 +2,12 @@ return {
   "supermaven-inc/supermaven-nvim",
   enabled = true,
   config = function()
-    require("supermaven-nvim").setup({})
+    local supermaven = require("supermaven-nvim")
+    supermaven.setup({})
+
+    -- vim.keymap.set("n", "<S-Tab>", supermaven.accept_suggestion, {})
+    -- vim.keymap.set("n", "<Tab>", supermaven.accept_suggestion, {})
+    vim.keymap.set("n", "<S-Tab>", function() supermaven.accept_suggestion() end, {})
+    vim.keymap.set("n", "<Tab>", function() supermaven.accept_suggestion() end, {})
   end,
 }
