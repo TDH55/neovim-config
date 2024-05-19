35e86e17933c142ac5e71c1fa9f8e11103741715
TDH55 <taylorhoward1@wustl.edu>
2024-05-19 11:32:02 -0500

    catpuccin to switch to when needing a light theme

---
+++ lua/config/lazy/catpuccin.lua
@@ -0,0 +1,9 @@
+return {
+  enabled = true,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  init = function()
+    -- vim.cmd('colorscheme catppuccin-latte')
+  end,
+}
--- lua/config/lazy/tokyonight.lua
+++ lua/config/lazy/tokyonight.lua
@@ -1,4 +1,5 @@
 return {
+  enabled = true,
   -- THEME
   "folke/tokyonight.nvim",
   lazy = false,
@@ -6,5 +7,6 @@ return {
   opts = {},
   init = function()
     vim.cmd('colorscheme tokyonight-night')
+    -- vim.cmd('colorscheme tokyonight-day')
   end,
 }
