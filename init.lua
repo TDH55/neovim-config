31272978a18824a85df6a5614f7d8f4eb3a92419
tdh55 <thoward@luckyorange.com>
2024-05-20 11:28:37 -0500

    bring over tab settings from old config

--- init.lua
+++ init.lua
@@ -103,7 +103,12 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
+vim.opt.smartindent = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
---
+++ lua/config/lazy/i18n.lua
@@ -0,0 +1,6 @@
+return {
+  "valen20Chx/nvim-i18n-tools",
+  config = function()
+    require("nvim-i18n-tools").setup()
+  end,
+}
